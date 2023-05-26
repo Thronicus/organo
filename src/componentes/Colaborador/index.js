76498780d111cab934fb3d090833036c7ff37098
Thronicus <thronicus@proton.me>
2023-05-26 01:22:50 -0300

    Alterações maiores: sistema de favoritos...

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,14 +1,36 @@
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
 import "./Colaborador.css";
 
-const Colaborador = ({ imagem, nome, cargo, corDeFundo }) => {
+const Colaborador = ({ time, colaborador, aoDeletar, aoFavoritar }) => {
+  const corBotao = {
+    "--cor-primaria": "rgba(255, 255, 255, 0.7)",
+    "--cor-pressionado": "#ffffff",
+  };
+
   return (
     <div className="colaborador">
-      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-        <img src={imagem} alt={nome}></img>
+      <div className="cabecalho" style={{ backgroundColor: time.cor }}>
+        <AiFillCloseCircle
+          className="botao-deletar"
+          onClick={() => aoDeletar(colaborador.id)}
+          style={corBotao}
+        />
+        <img src={colaborador.imagem} alt={colaborador.nome}></img>
       </div>
       <div className="rodape">
-        <h4>{nome}</h4>
-        <h5>{cargo}</h5>
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+        <div className="favoritar">
+          {colaborador.favorito ? (
+            <AiFillHeart
+              onClick={() => aoFavoritar(colaborador.id)}
+              style={{ color: "#ff0000" }}
+            />
+          ) : (
+            <AiOutlineHeart onClick={() => aoFavoritar(colaborador.id)} />
+          )}
+        </div>
       </div>
     </div>
   );
